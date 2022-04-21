4f0ad623771eb2a8fcdbffa9d50eb6e396da9c23
caglarylmz <caglar.ylmz@outlook.com>
2022-04-21 14:33:27 +0300

    example update

--- example/lib/main.dart
+++ example/lib/main.dart
@@ -1,4 +1,4 @@
-import 'package:example/home_page.dart';
+import 'package:awesome_number_picker/awesome_number_picker.dart';
 import 'package:example/scroll_behaviour.dart';
 import 'package:flutter/material.dart';
 
@@ -14,8 +14,101 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return const MaterialApp(
       title: 'Flutter Demo',
-      scrollBehavior: CustomScrollBehavior(),
+      scrollBehavior: CustomScrollBehavior(), //For web Scrolling
       home: HomePage(),
+    );
+  }
+}
+
+class HomePage extends StatefulWidget {
+  const HomePage({Key? key}) : super(key: key);
+
+  @override
+  State<HomePage> createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  int integerValue = 0;
+  double decimalValue = 0;
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: const Text("Awesome Number Picker")),
+      body: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        crossAxisAlignment: CrossAxisAlignment.center,
+        children: [
+          Row(
+            mainAxisAlignment: MainAxisAlignment.spaceAround,
+            crossAxisAlignment: CrossAxisAlignment.center,
+            children: [
+              const Text("Integer Value"),
+              Text(integerValue.toString()),
+            ],
+          ),
+          SizedBox(
+            height: 150,
+            child: IntegerNumberPicker(
+              initialValue: 5,
+              minValue: 0,
+              maxValue: 10,
+              onChanged: (i) => setState(() {
+                integerValue = i;
+              }),
+            ),
+          ),
+          SizedBox(
+            height: 150,
+            child: IntegerNumberPicker(
+              initialValue: 75,
+              minValue: 50,
+              maxValue: 100,
+              axis: Axis.horizontal,
+              onChanged: (i) => setState(() {
+                integerValue = i;
+              }),
+            ),
+          ),
+          Row(
+            mainAxisAlignment: MainAxisAlignment.spaceAround,
+            crossAxisAlignment: CrossAxisAlignment.center,
+            children: [
+              const Text("Decimal Value"),
+              Text(decimalValue.toStringAsFixed(3)),
+            ],
+          ),
+          SizedBox(
+            height: 150,
+            child: DecimalNumberPicker(
+              initialValue: 25.75,
+              minValue: 0,
+              maxValue: 100,
+              decimalPrecision: 3,
+              otherItemsDecoration: BoxDecoration(
+                boxShadow: [
+                  BoxShadow(
+                    color: Colors.grey.withOpacity(.5),
+                    blurRadius: 20.0, // soften the shadow
+                    spreadRadius: 0.0, //extend the shadow
+                  )
+                ],
+              ),
+              pickedItemDecoration: BoxDecoration(
+                boxShadow: [
+                  BoxShadow(
+                    color: Colors.blue.withOpacity(.5),
+                    blurRadius: 20.0, // soften the shadow
+                    spreadRadius: 0.0, //extend the shadow
+                  )
+                ],
+              ),
+              onChanged: (i) => setState(() {
+                decimalValue = i;
+              }),
+            ),
+          ),
+        ],
+      ),
     );
   }
 }
